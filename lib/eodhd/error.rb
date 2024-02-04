c375c845eef3ecd539d7a853a0ef719dafedccea
Bob Haupt <bob.haupt77@gmail.com>
2024-02-03 21:19:53 -0700

    Added files so that require 'eodhd' works

---
+++ lib/eodhd/error.rb
@@ -0,0 +1,34 @@
+module Eodhd
+  # Custom error class for rescuing from all Eodhd errors
+  class Error < StandardError
+    attr_reader :wrapped_exception
+
+    # @return [Hash]
+    def self.errors
+      @errors ||= descendants.each_with_object({}) do |klass, hash|
+        hash[klass::HTTP_STATUS_CODE] = klass if defined?(klass::HTTP_STATUS_CODE)
+        hash.update(klass.errors)
+      end
+    end
+
+    # @return [Array]
+    def self.descendants
+      ObjectSpace.each_object(::Class).select{|klass| klass < self}
+    end
+
+    # Initializes a new Error object
+    #
+    # @param exception [Exception, String]
+    # @param response_headers [Hash]
+    # @return [Eodhd::Error]
+    def initialize(exception=$!, response_headers={})
+      @wrapped_exception = exception
+      exception.respond_to?(:backtrace) ? super(exception.message) : super(exception.to_s)
+    end
+
+    def backtrace
+      @wrapped_exception.respond_to?(:backtrace) ? @wrapped_exception.backtrace : super
+    end
+
+  end
+end
